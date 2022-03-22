d4f28d570fa3fe99407d32a099481335787e6237
Duy Nguyen <you@example.com>
2022-03-22 15:41:52 +0700

    up

---
+++ c0821g1-sprint2/src/app/feature/employee/employee-create-acount/employee-create-acount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-create-acount',
+  templateUrl: './employee-create-acount.component.html',
+  styleUrls: ['./employee-create-acount.component.css']
+})
+export class EmployeeCreateAcountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ c0821g1-sprint2/src/app/feature/supplies/supplies.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SuppliesRoutingModule } from './supplies-routing.module';
+import { SuppliesComponent } from './supplies/supplies.component';
+
+
+@NgModule({
+  declarations: [SuppliesComponent],
+  imports: [
+    CommonModule,
+    SuppliesRoutingModule
+  ]
+})
+export class SuppliesModule { }
