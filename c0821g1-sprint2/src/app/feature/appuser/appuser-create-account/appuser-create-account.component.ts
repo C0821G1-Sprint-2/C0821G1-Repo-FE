6af9bedc0ba261ee86e2ff14e0db7efa209f4398
Gia Bảo <nguyenhoanggiabao1993@gmail.com>
2022-03-29 11:02:24 +0700

    29/03

---
+++ c0821g1-sprint2/src/app/feature/appuser/appuser-create-account/appuser-create-account.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-appuser-create-account',
+  templateUrl: './appuser-create-account.component.html',
+  styleUrls: ['./appuser-create-account.component.css']
+})
+export class AppuserCreateAccountComponent implements OnInit {
+  accountForm: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+}
