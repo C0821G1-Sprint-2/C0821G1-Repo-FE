e6123befe03d20bd1575cc94ee6a9a1d8d76d5d6
C0821G1-DangHuuDu <huud90@gmail.com>
2022-03-29 14:52:21 +0700

    DuDH up git

--- c0821g1-sprint2/src/app/service/employee.service.ts
+++ c0821g1-sprint2/src/app/service/employee.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Employee} from '../model/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  API_URL = 'http://localhost:8080/employee';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  findAllEmployee(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(this.API_URL + '/list');
+  }
 }
