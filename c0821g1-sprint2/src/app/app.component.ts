c2fcf2fe76ef062d486f66e235c19b4b7e27514a
C0721G2--L-V-N-Lanh <levanlanh1802@gmail.com>
2022-04-06 10:57:19 +0700

    up

--- c0821g1-sprint2/src/app/app.component.ts
+++ c0821g1-sprint2/src/app/app.component.ts
@@ -1,45 +1,9 @@
-import { Component, AfterViewInit } from '@angular/core';
-import * as Chart from 'chart.js';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'c0821g1-sprint2';
-  // title = 'chartjsangular';
-  canvas  : any;
-  ctx     : any;
-  canvas1 : any;
-  ctx1    : any;
-  canvas2 : any;
-  ctx2    : any;
-  canvas3 : any;
-  ctx3    : any;
-  ngOnInit() {
-    this.canvas = document.getElementById('myChart');
-    this.ctx = this.canvas.getContext('2d');
-    // @ts-ignore
-    let myChart = new Chart(this.ctx, {
-      type: 'pie',
-      data: {
-        labels: ["New", "In Progress", "On Hold"],
-        datasets: [{
-          label: '# of Votes',
-          data: [1, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: false,
-        display: true
-      }
-    });
-  }
 
 }
--- c0821g1-sprint2/src/app/feature/employee/employee-routing.module.ts
+++ c0821g1-sprint2/src/app/feature/employee/employee-routing.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {EmployeeCreateComponent} from './employee-create/employee-create.component';
+import {EmployeeEditComponent} from './employee-edit/employee-edit.component';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
 
 
 const routes: Routes = [
-  {path: 'list', component: EmployeeListComponent}
+  {
+    path: 'create', component: EmployeeCreateComponent
+  },
+  {
+    path: 'update/:id', component: EmployeeEditComponent
+  },
+  {
+    path: 'list', component: EmployeeListComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class EmployeeRoutingModule { }
+export class EmployeeRoutingModule {
+}
