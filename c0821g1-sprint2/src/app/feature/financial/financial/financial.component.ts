3da78eff2938cb980f25f80f6e5048f3af6d64b7
C0721G2--L-V-N-Lanh <levanlanh1802@gmail.com>
2022-04-08 08:27:08 +0700

    update

--- c0821g1-sprint2/src/app/feature/financial/financial/financial.component.ts
+++ c0821g1-sprint2/src/app/feature/financial/financial/financial.component.ts
@@ -1,11 +1,9 @@
-// @ts-ignore
 import {Component, OnInit,ViewChild, ElementRef} from '@angular/core';
 import {Financial} from "../../../model/financial";
 import {FinancialService} from "../../../service/financial.service";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import * as XLSX from "xlsx";
 import {Chart} from "../../../../assets/chart.js";
-
 @Component({
   selector: 'app-financial',
   templateUrl: './financial.component.html',
@@ -75,36 +73,8 @@ export class FinancialComponent implements OnInit {
 
     // chart
 
-    // this.canvas = document.getElementById('myChart');
-    // this.ctx = this.canvas.getContext('2d');
-    // let myChart = new Chart(this.ctx, {
-    //   type: 'pie',
-    //   data: {
-    //     labels: ["New", "In Progress", "On Hold"],
-    //     datasets: [{
-    //       label: '# of Votes',
-    //       data: [1, 2, 3],
-    //       backgroundColor: [
-    //         'rgba(255, 99, 132, 1)',
-    //         'rgba(54, 162, 235, 1)',
-    //         'rgba(255, 206, 86, 1)'
-    //       ],
-    //       borderWidth: 1
-    //     }]
-    //   },
-    //   options: {
-    //     responsive: false,
-    //     display: true
-    //   }
-    //
-    // }
-    // );
-    // console.log("aaaaaaaaaaaaaaaaaa" + myChart);
   }
 
-  searchEquipment(value: any) {
-    console.log(value);
-  }
 
   code() {
     this.month = this.date.controls.month.value;
